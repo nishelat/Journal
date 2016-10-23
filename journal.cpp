9718e931f318214f9ecf37b0803ed5b1ccfc996c
Nishir <nishelat@yahoo.com>
2016-10-22 17:20:42 -0700

    Add files via upload

--- journal.cpp
+++ journal.cpp
@@ -1,5 +1,5 @@
-// Journal program 
-// Created by Nishir Shelat
+// Journal program
+// Made by Nishir Shelat
 
 #include <iostream>
 #include <cstdlib>
@@ -10,7 +10,20 @@
 
 using namespace std;
 
-// Prints the main menu
+// Returns false if the entered password matches the given password
+bool checkPassword(string givenPassword) {
+	// Ask the user for the password
+	cout << "Please enter the password (case-sensitive): ";
+	// Store and compare tha passwords
+	string inputPassword;
+	getline(cin,inputPassword);
+	if(inputPassword == givenPassword) {
+		return false;
+	}
+	return true;
+}
+
+// Prints the menu
 void printMenu() {
 	system("cls");
 	cout << "Welcome to the C++ Journal" << endl;
@@ -22,165 +35,181 @@ void printMenu() {
 	cout << ">> ";
 }
 
-// Return true if the given title already exists or is invalid
-bool checkTitle(string inputTitle) {
-	// Check if the title is valid
-	if(inputTitle.length() == 0 || inputTitle.length() > 40) {
-		cout << endl << "Error: either that title already exists or it is invalid" << endl;
-		return true;
-	}
-	// Open an input stream for the journal file
-	ifstream journal;
-	journal.open("journal.txt");
-	// Scan through the file and return true if the title is found
-	string currentTitle;
-	int len = inputTitle.length();
-	while(getline(journal,currentTitle)) {
-		if(currentTitle.substr(0,len) == inputTitle) {
-			cout << endl << "Error: either that title already exists or it is invalid" << endl << endl;
-			// Close the stream
-			journal.close();
-			return true;
-		}
-	}
-	// Close the stream
-	journal.close();
-	return false;
+// Takes an int and returns the string form
+string int_to_string(int i) {
+	// Open a string stream
+	stringstream int_to_string;
+	int_to_string << i;
+	// Return the converted value
+	return int_to_string.str();
 }
 
-// Searches through the lexicon file for a word and returns true if it exists
-bool wordCheck(string word) {
-	// Open a stream
-	fstream lexicon;
-	lexicon.open("lexicon.txt");
-	string temp;
-	while(getline(lexicon,temp)) {
-		if(temp == word) {
-			lexicon.close();
-			return true;
-		}
-	}
-	lexicon.close();
-	return false;
-}
-
-// Checks if a given word is in the English language
-string spellCheck(string word) {
-	cin.ignore();
-	// Check if length is 1
-	if(word.length() < 2) {
-		return word;
-	}
-	// Check for a lowercase, English character in the beginning
-	if(!(word[0] >= 97 && word[0] <= 122)) {
-		return word;
-	}
-	if(word[word.length() - 1] == '.' || word[word.length() - 1] == ',' || word[word.length() - 1] == '?' || word[word.length() - 1] == '!') {
-		word = word.substr(0,word.length() - 1);
-	}
-	if(wordCheck(word)) {
-		return word;
-	}
-	cout << "The following word is spelled wrong: " << word << endl;
-	return word;
-}
-
-// Write a new entry to the journal file
-void writeEntry() {
-	// Create a stream for the journal file
-	ofstream journal;
-	journal.open("journal.txt", ios::app);
-	// Prompt the user for a title
-	string title;
-	system("cls");
-	// Check if the title is valid or already exists
-	do {
-		cout << "Please enter title for your new entry (max 40 characters): ";
-		cin.clear();
-		getline(cin,title);
-	}
-	while(checkTitle(title));
-	// Create the new entry in the file
-	journal << title;
-	journal << " -entryStart ";
-	// Prompt the user to enter their entry
-	string entry;
-	system("cls");
-	cout << title << endl << endl;
-	cout << "Please type in your entry. When you are done, type \"EXIT\" to quit." << endl << endl;
-	while(cin >> entry) {
-		if(entry == "EXIT") {
+// Reads all the current files and returns the next free int
+int findInt() {
+	int i = 1;
+	// Loop until testStream fails
+	while(1) {
+		string name = int_to_string(i) + ".txt";
+		fstream testStream;
+		testStream.open(name.c_str());
+		if(!testStream) {
 			break;
 		}
+		i++;
+	}
+	return i;
+}
+
+// Creates a text file for the next available int
+void createFile(int i) {
+	string name = int_to_string(i) + ".txt";
+	ofstream journal;
+	journal.open(name.c_str());
+	journal.close();
+}
+
+// Encrypts a string
+string encryptString(string entry) {
+	for(int i = 0; i < entry.length(); i++) {
+		entry[i] += 1;
+	}
+	return entry;
+}
+
+// Decrypts a string
+string decryptString(string entry) {
+	for(int i = 0; i < entry.length(); i++) {
+		entry[i] -= 1;
+	}
+	return entry;
+}
+
+// Decrypts a char
+char decryptChar(char entry) {
+	entry -= 1;
+	return entry;
+}
+
+// Returns true if an entry meets the min requirements
+bool checkEntry(int i) {
+	// Open the file stream
+	string name = int_to_string(i) + ".txt";
+	ifstream journal;
+	journal.open(name.c_str());
+	// Check for a minimum of 15 chars
+	char temp;
+	int counter = 0;
+	while(journal >> temp) {
+		if(journal.fail()) {
+			break;
+		}
+		counter++;
+	}
+	if(counter < 15) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+// Writes an entry
+void writeEntry() {
+	// Clear the screen
+	system("cls");
+	// Create the new file
+	int i = findInt();
+	createFile(i);
+	// Open the file
+	string name = int_to_string(i) + ".txt";
+	ofstream journal;
+	journal.open(name.c_str());
+	// Prompt the user for the entry
+	cout << "Please type in your journal entry (min 15 characters)." << endl;
+	cout << "Enter \"EXIT\" when you are done." << endl;
+	cout << "Note that any text entered after typing \"EXIT\" will not be saved." << endl << endl;
+	string entry;
+	while(entry.find(encryptString("EXIT")) == -1) {
+		getline(cin,entry);
+		if(entry.find("EXIT") != -1) {
+			entry = entry.substr(0,entry.find("EXIT"));
+			entry = encryptString(entry);
+			journal << entry << " ";
+			break;
+		}
+		entry = encryptString(entry);
 		journal << entry << " ";
 	}
-	journal << "-entryEnd" << endl;
-	// Close the stream
 	journal.close();
-}
-
-// Print all the existing titles
-void printTitles() {
-	// Create a stream for the file
-	fstream journal;
-	journal.open("journal.txt");
-	// Display a full list of titles for the user
-	system("cls");
-	cout << "Here is a list of all the current entries:" << endl << endl;
-	string title;
-	while(journal >> title) {
-		if(title != "-entryStart") {
-			cout << title << " ";
-		}
-		else {
-			cout << endl;
-			while(journal >> title) {
-				if(title == "-entryEnd") {
-					break;
-				}
-			}
-		}
-	}
-	cout << endl;
-	journal.close();
-}
-
-// Prints an entry with the given title and returns false if the entry doesn't exist
-bool printEntry(string title) {
-	// Create a stream
-	fstream journal;
-	journal.open("journal.txt");
-	// Find the entry the user wants
-	int len = title.length();
-	string content;
-	while(getline(journal,content)) {
-		if(content.substr(0,len) == title) {
-			cout << endl;
-			cout << content.substr(len + 13, content.length() - 10 - (len + 13)) << endl << endl;
-			system("pause");
-			return true;
-		}
-	}
-	return false;
-	// Close the stream
-	journal.close();
-}
-
-// Read an entry from the journal file
-void readEntry() {
-	printTitles();
-	// Ask the user to enter the title they want to see
-	cout << "Please enter the title of the entry you wish to read: ";
-	string title;
-	getline(cin,title);
-	// Print the entry or an error message
-	if(!printEntry(title)) {
-		cout << "Sorry, that title doesn't match any entry." << endl << endl;
+	if(!checkEntry(i)) {
+		// Print an error message and delete the file
+		system("cls");
+		cout << "Error - An entry must have at least 15 characters. Your file has not been saved." << endl << endl;
+		remove(name.c_str());
 		system("pause");
 	}
 }
 
+// Given an int, opens the corresponding file and returns it
+string loadEntry(int i) {
+	// Open the file
+	string name = int_to_string(i) + ".txt";
+	ifstream journal;
+	journal.open(name.c_str());
+	string entry;
+	getline(journal,entry);
+	return entry;
+}
+
+// Prints all the current entry previews
+void printPreviews() {
+	// Open each entry and print the first 15 chars
+	for(int i = 1; i < findInt(); i++) {
+		string name = int_to_string(i) + ".txt";
+		ifstream journal;
+		journal.open(name.c_str());
+		cout << i << ") \"";
+		for(int j = 0; j < 15; j++) {
+			char entry;
+			journal >> entry;
+			cout << decryptChar(entry);
+		}
+		cout << "...\"" << endl;
+	}
+}
+
+// Reads an entry
+void readEntry() {
+	// Clear the screen
+	system("cls");
+	// Display the current entries
+	printPreviews();
+	cout << endl;
+	cout << "Please type in the number that corresponds to the entry you wish to read." << endl << endl;
+	cout << ">> ";
+	string choice;
+	getline(cin,choice);
+	int i = atoi(choice.c_str());
+	if(!(i > 0 && i < findInt())) {
+		// Error message and quit
+		system("cls");
+		cout << "Error - That is not a valid entry number." << endl << endl;
+		system("pause");
+		return;
+	}
+	system("cls");
+	string entry = decryptString(loadEntry(i));
+	entry = entry.substr(0,entry.length()-2);
+	cout << entry;
+	cout << endl << endl;
+	system("pause");
+}
+
 int main() {
+	// Ask for a password
+	while(checkPassword("password")) {
+		cout << endl << "Incorrect, try again." << endl << endl;
+	}
 	// Print the menu until a valid option is selected
 	string response;
 	do {
